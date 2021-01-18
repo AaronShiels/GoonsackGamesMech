a97a3fe7c8fc774d137ceeffc1411809dc683212
Aaron Shiels <aaron.shiels@outlook.com>
2021-01-17 21:06:46 -0800

    Initial commit, basic movement

---
+++ src/Cyborg.Core/MainGame.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Cyborg.Core
+{
+    public class MainGame : Game
+    {
+        // Core
+        private readonly GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // Game
+        private Player _player;
+
+        public MainGame()
+        {
+            _graphics = new GraphicsDeviceManager(this)
+            {
+                IsFullScreen = false
+            };
+
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _player = new Player(Content);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+
+            _player.Update(gameTime, keyboardState);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _player.Draw(gameTime, _spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ src/Cyborg.DesktopGL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cyborg.Core;
+
+namespace Cyborg.DesktopGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+                game.Run();
+        }
+    }
+}
