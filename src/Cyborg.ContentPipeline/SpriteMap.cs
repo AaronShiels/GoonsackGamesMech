acf2505fed1d97e843e52444c8238979b403e1a2
Aaron Shiels <ashiels@pof.com>
2021-01-27 18:24:11 -0800

    Align sprite map components with inverted pattern

--- src/Cyborg.ContentPipeline/SpriteMap.cs
+++ src/Cyborg.ContentPipeline/SpriteMap.cs
@@ -1,31 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Cyborg.ContentPipeline
 {
     [Serializable]
     public class SpriteMap
     {
+        public string SpriteSheet { get; set; }
         public short TileWidth { get; set; }
         public short TileHeight { get; set; }
-        public IList<LayerDefinition> Layers { get; set; }
-        public TileSetDefinition TileSet { get; set; }
-
-        [Serializable]
-        public class LayerDefinition
-        {
-            public string Name { get; set; }
-            public short Width { get; set; }
-            public short Height { get; set; }
-            public short[,] Values { get; set; }
-        }
-
-        [Serializable]
-        public class TileSetDefinition
-        {
-            public short TileCount { get; set; }
-            public short Columns { get; set; }
-            public string Resource { get; set; }
-        }
+        public short Width { get; set; }
+        public short Height { get; set; }
+        public short[,] BackgroundMap { get; set; }
     }
 }
--- src/Cyborg/Entities/Area.cs
+++ src/Cyborg/Entities/Area.cs
@@ -1,32 +1,13 @@
 using Cyborg.Components;
-using Cyborg.ContentPipeline;
-using Cyborg.Sprites;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace Cyborg.Entities
 {
     public class Area : ISpriteMap
     {
-        private readonly SpriteMapContainer _spriteMap;
-
-        public Area(ContentManager contentManager)
-        {
-            _spriteMap = CreateSpriteMap(contentManager);
-        }
-
-        public SpriteMapContainer SpriteMapContainer => _spriteMap;
         public Vector2 Position { get; set; } = Vector2.Zero;
         public Vector2 Size { get; set; } = new Vector2(320, 176);
+        public string SpriteMap => "demo_map";
         public bool Destroyed { get; set; }
-
-        private SpriteMapContainer CreateSpriteMap(ContentManager contentManager)
-        {
-            var mapDefinition = contentManager.Load<SpriteMap>("demo_map");
-            var tileSetTexture = contentManager.Load<Texture2D>(mapDefinition.TileSet.Resource);
-
-            return new SpriteMapContainer(mapDefinition, tileSetTexture);
-        }
     }
 }
