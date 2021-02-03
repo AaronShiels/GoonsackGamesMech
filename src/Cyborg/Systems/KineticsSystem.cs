3146811b462067d09d6e8738b9b9e05ec17ef4d0
Aaron Shiels <ashiels@pof.com>
2021-02-02 19:30:03 -0800

    Apply velocity changes to collisions

--- src/Cyborg/Systems/KineticsSystem.cs
+++ src/Cyborg/Systems/KineticsSystem.cs
@@ -18,8 +18,9 @@ namespace Cyborg.Systems
 
             foreach (var entity in entities.OfType<IKinetic>())
             {
-                if (Debug.Enabled)
-                    Debug.Add(entity, "DrivingForce", $"{entity.Force.Length():F}");
+                // Apply stopping thresholds
+                if (entity.Velocity.Length() < _stoppingThreshold)
+                    entity.Velocity = Vector2.Zero;
 
                 // Apply drag to force
                 // Fd = kv²
@@ -29,41 +30,15 @@ namespace Cyborg.Systems
                     ? -dragScalar * Vector2.Normalize(entity.Velocity)
                     : Vector2.Zero;
 
-                if (Debug.Enabled)
-                    Debug.Add(entity, "DragForce", $"{dragScalar:F}");
-
                 entity.Force += dragForce;
-
-                if (Debug.Enabled)
-                    Debug.Add(entity, "Force", $"{entity.Force.Length():F}");
 
                 // Apply force to velocity
                 // F = ma, a = F/m
                 var acceleration = entity.Force / entity.Mass;
                 entity.Velocity += acceleration * elapsed;
 
-                // Apply stopping thresholds
-                if (entity.Velocity.Length() < _stoppingThreshold)
-                    entity.Velocity = Vector2.Zero;
-
-                if (Debug.Enabled)
-                    Debug.Add(entity, "Velocity", $"{entity.Velocity.Length():F}");
-
                 // Apply velocity
                 entity.Position += entity.Velocity * elapsed;
-
-                if (Debug.Enabled)
-                    Debug.Add(entity, "Position", $"{entity.Position.X:F}, {entity.Position.Y:F}");
-
-                // Apply collision
-                // var horizontalCollision = entity.Position.X - entity.Size.X / 2 < 0 || entity.Position.X + entity.Size.X / 2 > Constants.BaseWidth;
-                // var verticalCollision = entity.Position.Y - entity.Size.Y / 2 < 0 || entity.Position.Y + entity.Size.Y / 2 > Constants.BaseHeight;
-
-                // if (horizontalCollision || verticalCollision)
-                // {
-                //     entity.Position = new Vector2(MathHelper.Clamp(entity.Position.X, entity.Size.X / 2, Constants.BaseWidth - entity.Size.X / 2), MathHelper.Clamp(entity.Position.Y, entity.Size.Y / 2, Constants.BaseHeight - entity.Size.Y / 2));
-                //     entity.Velocity = new Vector2(horizontalCollision ? 0 : entity.Velocity.X, verticalCollision ? 0 : entity.Velocity.Y);
-                // }
             }
         }
     }
