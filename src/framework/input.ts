cb6afa72cf05299f86e31efdd0c8f921e4ea31d9
Aaron Shiels <ashiels@pof.com>
2021-03-29 11:37:34 -0700

    Implement attacks with touch controls

--- src/framework/input.ts
+++ src/framework/input.ts
@@ -1,16 +1,14 @@
+import { cloneDeep } from "lodash";
 import { hasValue, normalise, subtract, Vector } from "../shapes";
 import game from "./Game";
 
 interface GameInput {
 	moveDirection: Vector;
-	attack: GameButton;
-	dash: GameButton;
+	attack: boolean;
+	dash: boolean;
 }
 
-interface GameButton {
-	pressed: boolean;
-	held: boolean;
-}
+const tapThreshold = 0.2 * 1000;
 
 const isTouch: boolean = "ontouchstart" in window || !!navigator.maxTouchPoints || !!navigator.msMaxTouchPoints;
 
@@ -22,22 +20,47 @@ enum Keys {
 	Space = " "
 }
 
-const currentKeyboardInput: Record<Keys, boolean> = {
-	w: false,
-	a: false,
-	s: false,
-	d: false,
-	" ": false
-};
-let previousKeyboardInput = Object.assign({}, currentKeyboardInput);
+interface KeyState {
+	down: number;
+	up: number;
+}
 
-const currentMouseInput = { position: { x: 0, y: 0 }, clickDown: false };
-let previousMouseInput = Object.assign({}, currentMouseInput);
+const isDown = (current: KeyState): boolean => current.down > current.up;
+const isPressed = (current: KeyState, previous: KeyState): boolean => current.down > current.up && previous.down <= previous.up;
+const isHeld = (current: KeyState): boolean => current.down > current.up && performance.now() > current.down + tapThreshold;
+const isTapped = (current: KeyState, previous: KeyState): boolean =>
+	current.up > current.down && previous.up <= previous.down && current.up - current.down < tapThreshold;
+
+interface KeyboardInput extends Record<Keys, KeyState> {
+	w: KeyState;
+	a: KeyState;
+	s: KeyState;
+	d: KeyState;
+	" ": KeyState;
+}
+
+const currentKeyboardInput: KeyboardInput = {
+	w: { down: 0, up: 0 },
+	a: { down: 0, up: 0 },
+	s: { down: 0, up: 0 },
+	d: { down: 0, up: 0 },
+	" ": { down: 0, up: 0 }
+};
+let previousKeyboardInput: KeyboardInput = cloneDeep(currentKeyboardInput);
+
+interface MouseInput {
+	position: Vector;
+	click: KeyState;
+}
+
+const currentMouseInput: MouseInput = { position: { x: 0, y: 0 }, click: { down: 0, up: 0 } };
+let previousMouseInput: MouseInput = cloneDeep(currentMouseInput);
 
 const handleKeyDown = (event: KeyboardEvent): void => {
 	if (!Object.keys(currentKeyboardInput).includes(event.key)) return;
 
-	currentKeyboardInput[event.key as Keys] = true;
+	const key = currentKeyboardInput[event.key as Keys];
+	if (!key.down || !isDown(key)) key.down = performance.now();
 
 	event.preventDefault();
 };
@@ -45,13 +68,14 @@ const handleKeyDown = (event: KeyboardEvent): void => {
 const handleKeyUp = (event: KeyboardEvent): void => {
 	if (!Object.keys(currentKeyboardInput).includes(event.key)) return;
 
-	currentKeyboardInput[event.key as Keys] = false;
+	const key = currentKeyboardInput[event.key as Keys];
+	key.up = performance.now();
 
 	event.preventDefault();
 };
 
 const handlePointerDown = (event: PointerEvent): void => {
-	currentMouseInput.clickDown = true;
+	currentMouseInput.click.down = performance.now();
 
 	event.preventDefault();
 };
@@ -64,7 +88,7 @@ const handlePointerMove = (event: PointerEvent): void => {
 };
 
 const handlePointerUp = (event: PointerEvent): void => {
-	currentMouseInput.clickDown = false;
+	currentMouseInput.click.up = performance.now();
 
 	event.preventDefault();
 };
@@ -78,29 +102,30 @@ window.addEventListener("pointermove", handlePointerMove, false);
 
 const getInput = (reference: Vector): GameInput => {
 	let moveDirection: Vector = { x: 0, y: 0 };
-	let attack: GameButton = { pressed: false, held: false };
-	let dash: GameButton = { pressed: false, held: false };
+	let attack: boolean = false;
+	let dash: boolean = false;
 
 	if (isTouch) {
-		const relativeMousePosition = subtract(currentMouseInput.position, reference);
-		if (hasValue(relativeMousePosition)) moveDirection = normalise(relativeMousePosition);
+		attack = isTapped(currentMouseInput.click, previousMouseInput.click);
+
+		if (isHeld(currentMouseInput.click) || attack) {
+			const relativeMousePosition = subtract(currentMouseInput.position, reference);
+			if (hasValue(relativeMousePosition)) moveDirection = normalise(relativeMousePosition);
+		}
 	} else {
 		const keyboardInputVector = { x: 0, y: 0 };
-		if (currentKeyboardInput.d) keyboardInputVector.x++;
-		if (currentKeyboardInput.a) keyboardInputVector.x--;
-		if (currentKeyboardInput.s) keyboardInputVector.y++;
-		if (currentKeyboardInput.w) keyboardInputVector.y--;
+		if (isDown(currentKeyboardInput.d)) keyboardInputVector.x++;
+		if (isDown(currentKeyboardInput.a)) keyboardInputVector.x--;
+		if (isDown(currentKeyboardInput.s)) keyboardInputVector.y++;
+		if (isDown(currentKeyboardInput.w)) keyboardInputVector.y--;
 		if (hasValue(keyboardInputVector)) moveDirection = normalise(keyboardInputVector);
 
-		attack.held = currentMouseInput.clickDown;
-		attack.pressed = currentMouseInput.clickDown && !previousMouseInput.clickDown;
-
-		dash.held = currentKeyboardInput[Keys.Space];
-		dash.pressed = currentKeyboardInput[Keys.Space] && !previousKeyboardInput[Keys.Space];
+		attack = isPressed(currentMouseInput.click, previousMouseInput.click);
+		dash = isPressed(currentKeyboardInput[Keys.Space], previousKeyboardInput[Keys.Space]);
 	}
 
-	Object.assign(previousKeyboardInput, currentKeyboardInput);
-	Object.assign(previousMouseInput, currentMouseInput);
+	previousKeyboardInput = cloneDeep(currentKeyboardInput);
+	previousMouseInput = cloneDeep(currentMouseInput);
 
 	return { moveDirection, attack, dash };
 };
